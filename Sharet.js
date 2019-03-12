2b5e75025ee36a12144fb6447cdd94b2be395ba8
3lawy90k <48464066+3lawy90k@users.noreply.github.com>
2019-03-12 06:41:39 +0300

    Create Sharet.js

---
+++ Sharet.js
@@ -0,0 +1,15 @@
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #8F4651;font-family: jazeera-light, FontAwesome;border-bottom: 3px solid #8F4651;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"style="
+    font-family: jazeera-light, FontAwesome;
+" >  عزيزي الزائـر اضغط هنـا </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" >  قوانين وشروط شات زهره اللوتس للجوال
+</p>
+<div style="float: right;padding-right: 20px;">- 1 </div> يرجى من الاداريين و المراقبين عدم تُرْك الغرف العامه بلا مراقبين ' و عدم ترويج اسم موقع مشابه من الزوار او الامراقبين
+</p>
+<div style="float: right;padding-right: 20px;">- 2</div> يمنع منعاً باتّاً مِن المراقبين او الاعضاء ذكر او ترويج اسم موقع مشابه اخر لـ سبب اي ظرف ' او سيتم التعرض لحذف العضويه او الحظر الدائم 
+</p>
+<div style="float: right;padding-right: 20px;">- 3</div> عزيزي الزائر عند تسجيلك  يتم ثبات الهدايا والحاله والعديد من المزايا الاخرى
+</p>
+<div style="float: right;padding-right: 20px;">- 4</div> عدم التحدث و فتح مواضيع عن السياسه او الأديان او المذاهب و ماشابه العنصريه و غيرها 
+
+</p>
+في حال وجود شكوى على احد الاعضاء او الاداريين لا تتردد ف التوجه للاداره مع اخذ صوره للحدث  "  •• إدارة شات زهره اللوتس للجوال 🌹🌹
+ </div> </div></div><center>`).insertBefore('#d2');
